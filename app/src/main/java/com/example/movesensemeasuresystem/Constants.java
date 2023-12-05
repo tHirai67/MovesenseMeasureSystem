5bee540e9bb79ae4953e8ce8aaf3623712f62d53
Tatsuhiko Hirai <thirai67lmbj@gmail.com>
2023-12-05 19:11:58 +0900

    角速度計測(途中2)

--- app/src/main/java/com/example/movesensemeasuresystem/Constants.java
+++ app/src/main/java/com/example/movesensemeasuresystem/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
     public static final String URI_MEAS_ACC_52 = "/Meas/Acc/416";
     public static final String URI_MEAS_ACC_102 = "/Meas/Acc/833";
     public static final String URI_MEAS_ACC_200 = "/Meas/Acc/1666";
+    public static final String IMU6_PATH = "/Meas/IMU6/833";
+    public static final String IMU9_PATH = "/Meas/IMU9/";
 }
--- app/src/main/java/com/example/movesensemeasuresystem/activity_record/RecordActivity.java
+++ app/src/main/java/com/example/movesensemeasuresystem/activity_record/RecordActivity.java
@@ -13,6 +13,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.movesensemeasuresystem.Constants;
+import com.example.movesensemeasuresystem.ImuModel;
 import com.example.movesensemeasuresystem.activity_connection.MovesenseModel;
 import com.example.movesensemeasuresystem.R;
 import com.example.movesensemeasuresystem.activity_connection.ConnectedListMovesense;
@@ -35,28 +36,22 @@ public class RecordActivity extends AppCompatActivity {
     private static ArrayList<MovesenseModel> movesenseList;
 
     //Movesense lib
-    private MdsSubscription mdsSubscription1_acc;
-    private MdsSubscription mdsSubscription1_gyro;
-    private MdsSubscription mdsSubscription2_acc;
-    private MdsSubscription mdsSubscription2_gyro;
+    private MdsSubscription mdsSubscription1;
+    private MdsSubscription mdsSubscription2;
     private Mds mds;
 
     //info
     private long first_time;
 
     //file
-    private File file_acc_1;
-    private File file_gyro_1;
-    private File file_acc_2;
-    private File file_gyro_2;
+    private File file1;
+    private File file2;
 
-    private final String ANGULAR_VELOCITY_PATH = "Meas/Gyro/";
-    private int sampleRate = 52;
+    private final String IMU6_PATH = "Meas/IMU6/";
+    private final String IMU9_PATH = "Meas/IMU9/";
 
-    private CsvLogger csvLogger_acc_1;
-    private CsvLogger csvLogger_gyro_1;
-    private CsvLogger csvLogger_acc_2;
-    private CsvLogger csvLogger_gyro_2;
+    private CsvLogger csvLogger1;
+    private CsvLogger csvLogger2;
 
     private boolean isRecord;
 
@@ -124,232 +119,142 @@ public class RecordActivity extends AppCompatActivity {
                 String currentTimestamp = formatter.format(date);
                 //file name1
                 // timestamp + device serial + data type,
-                StringBuilder sb_acc_1 = new StringBuilder();
-                StringBuilder sb_gyro_1 = new StringBuilder();
-                StringBuilder sb_acc_2 = new StringBuilder();
-                StringBuilder sb_gyro_2 = new StringBuilder();
+                StringBuilder sb1 = new StringBuilder();
+                StringBuilder sb2 = new StringBuilder();
 
-                sb_acc_1.append(movesenseList.get(0).getSerial()).append("_").append(spinner1.getSelectedItem().toString()).append("_").append(currentTimestamp).append("_acc.csv");
-                sb_gyro_1.append(movesenseList.get(0).getSerial()).append("_").append(spinner1.getSelectedItem().toString()).append("_").append(currentTimestamp).append("_gyro.csv");
-                sb_acc_2.append(movesenseList.get(0).getSerial()).append("_").append(spinner1.getSelectedItem().toString()).append("_").append(currentTimestamp).append("_acc.csv");
-                sb_gyro_2.append(movesenseList.get(0).getSerial()).append("_").append(spinner1.getSelectedItem().toString()).append("_").append(currentTimestamp).append("_gyro.csv");
+                sb1.append(movesenseList.get(0).getSerial()).append("_").append(spinner1.getSelectedItem().toString()).append("_").append(currentTimestamp).append("_acc.csv");
+                sb2.append(movesenseList.get(0).getSerial()).append("_").append(spinner1.getSelectedItem().toString()).append("_").append(currentTimestamp).append("_acc.csv");
 
                 Context context = getApplicationContext();
-                file_acc_1 = new File(context.getFilesDir(),sb_acc_1.toString());
-                file_gyro_1 = new File(context.getFilesDir(),sb_gyro_1.toString());
-                file_acc_2 = new File(context.getFilesDir(),sb_acc_2.toString());
-                file_gyro_2 = new File(context.getFilesDir(),sb_gyro_2.toString());
+                file1 = new File(context.getFilesDir(),sb1.toString());
+                file2 = new File(context.getFilesDir(),sb2.toString());
 
-                csvLogger_acc_1 = new CsvLogger(file_acc_1);
-                csvLogger_gyro_1 = new CsvLogger(file_gyro_1);
-                csvLogger_acc_2 = new CsvLogger(file_acc_2);
-                csvLogger_gyro_2 = new CsvLogger(file_gyro_2);
+                csvLogger1 = new CsvLogger(file1);
+                csvLogger2 = new CsvLogger(file2);
 
                 isRecord = false;
                 first_time=System.currentTimeMillis();
-                subscribeToSensor2(movesenseList.get(1).getSerial());
+                //subscribeToSensor2(movesenseList.get(1).getSerial());
                 subscribeToSensor1(movesenseList.get(0).getSerial());
                 btnRecord.setText("Stop Recording");
             }else{
                 isRecord = true;
-                unsubscribe1_acc();
-                unsubscribe1_gyro();
-                unsubscribe2_acc();
-                unsubscribe2_gyro();
+                unsubscribe1();
+                unsubscribe2();
                 btnRecord.setText("Start Recording");
-                csvLogger_acc_1.finishSavingLogs();
-                csvLogger_gyro_1.finishSavingLogs();
-                csvLogger_acc_2.finishSavingLogs();
-                csvLogger_gyro_2.finishSavingLogs();
+                csvLogger1.finishSavingLogs();
+                csvLogger2.finishSavingLogs();
             }
         }
     }
 
     private void subscribeToSensor1(String deviceSerial1) {
         // //Movesense1の加速度の登録
-        if (mdsSubscription1_acc != null) {
-            unsubscribe1_acc();
+        if (mdsSubscription1 != null) {
+            unsubscribe1();
         }
-        String accUri = Constants.URI_MEAS_ACC_52;
-        String strContract_acc_1 = "{\"Uri\": \"" + deviceSerial1 + accUri + "\"}";
-
-        mdsSubscription1_acc = Mds.builder().build(this).subscribe(Constants.URI_EVENTLISTENER, strContract_acc_1, new MdsNotificationListener() {
+        String Uri = Constants.IMU6_PATH;
+        String strContract2 = "{\"Uri\": \"" + deviceSerial1 + Uri + "\"}";
+        mdsSubscription2 = Mds.builder().build(this).subscribe(Constants.URI_EVENTLISTENER, strContract2, new MdsNotificationListener() {
 
             // センサ値を受け取るメソッド
             @Override
             public void onNotification(String data) {
-                MovesenseAccDataResponse accResponse = new Gson().fromJson(data, MovesenseAccDataResponse.class);
-
-                if (accResponse != null && accResponse.body.array.length > 0) {
-                    StringBuffer sb = new StringBuffer();
-                    double time = System.currentTimeMillis() - first_time;
-                    double x = accResponse.body.array[0].x;
-                    double y = accResponse.body.array[0].y;
-                    double z = accResponse.body.array[0].z;
-                    Log.d(TAG, deviceSerial1 + "\n経過時刻Time:" + String.valueOf(time)
-                            + "\nsensorTime:" + String.valueOf(accResponse.body.timestamp)
-                            + "\nx:" + String.valueOf(x)
-                            + "\ny:" + String.valueOf(y)
-                            + "\nz:" + String.valueOf(z));
-                    sb.append("経過時刻:" + (double) Math.round((double) time / 10) / 100 + "秒");
-                    sb.append("\n");
-                    sb.append("x:" + x);
-                    sb.append("\n");
-                    sb.append("y:" + y);
-                    sb.append("\n");
-                    sb.append("z:" + z);
-                    tvSensorInfo1.setText(sb.toString());
-                    csvLogger_acc_1.appendHeader("Sensor time (ms),System time (ms),X (m/s^2),Y (m/s^2),Z (m/s^2)");
-                    csvLogger_acc_1.appendLine(String.format(Locale.getDefault(), "%d,%.6f,%.6f,%.6f,%.6f", accResponse.body.timestamp, time / 1000, x, y, z));
-                }
+                ImuModel imuModel = new Gson().fromJson(data, ImuModel.class);
+                double time=System.currentTimeMillis()-first_time;
+                double x = imuModel.getBody().getArrayAcc()[0].getX();
+                double y = imuModel.getBody().getArrayAcc()[0].getY();
+                double z = imuModel.getBody().getArrayAcc()[0].getZ();
+                double gx = imuModel.getBody().getArrayAcc()[0].getX();
+                double gy = imuModel.getBody().getArrayAcc()[0].getY();
+                double gz = imuModel.getBody().getArrayAcc()[0].getZ();
+                Log.d(TAG,deviceSerial1+"\n経過時刻Time:"+ String.valueOf(time)
+                        +"\nx:"+String.valueOf(x)
+                        +"\ny:"+ String.valueOf(y)
+                        +"\nz:"+String.valueOf(z)
+                        +"\ngx:"+String.valueOf(gx)
+                        +"\ngy:"+ String.valueOf(gy)
+                        +"\ngz:"+String.valueOf(gz));
+//                sb.append("経過時刻:"+(double)Math.round((double)time/10)/100+"秒");
+//                sb.append("\n");
+//                sb.append("x:"+x);
+//                sb.append("\n");
+//                sb.append("y:"+y);
+//                sb.append("\n");
+//                sb.append("z:"+z);
+                //tvSensorInfo2.setText(sb.toString());
+//                csvLogger2.appendHeader("Sensor time (ms),System time (ms),X (m/s^2),Y (m/s^2),Z (m/s^2)");
+//                csvLogger2.appendLine(String.format(Locale.getDefault(), "%d,%.6f,%.6f,%.6f,%.6f", imuModel.body.timestamp, time/1000,x,y,z));
             }
 
             @Override
             public void onError(MdsException error) {
                 Log.e(TAG, "subscription onError(): ", error);
-                unsubscribe1_acc();
-                onDestroy();
-            }
-        });
-
-        //Movesense1の角速度の登録
-        if (mdsSubscription1_gyro != null) {
-            unsubscribe1_gyro();
-        }
-        String gyroUri = ANGULAR_VELOCITY_PATH + sampleRate;
-        String strContract_gyro_1 =  "{\"Uri\": \"" + deviceSerial1 + gyroUri+  "\"}";
-
-        mdsSubscription1_gyro = Mds.builder().build(this).subscribe(Constants.URI_EVENTLISTENER, FormatHelper.formatContractToJson(deviceSerial1, ANGULAR_VELOCITY_PATH + sampleRate), new MdsNotificationListener() {
-
-            // センサ値を受け取るメソッド
-            @Override
-            public void onNotification(String data) {
-                MovesenseGyroDataResponse gyroResponse = new Gson().fromJson(data, MovesenseGyroDataResponse.class);
-
-                if (gyroResponse != null & gyroResponse.body.array.length > 0) {
-                    double time=System.currentTimeMillis()-first_time;
-                    double x = gyroResponse.body.array[0].x;
-                    double y = gyroResponse.body.array[0].y;
-                    double z = gyroResponse.body.array[0].z;
-                    csvLogger_gyro_1.appendHeader("Sensor time (ms),System time (ms),X (m/s^2),Y (m/s^2),Z (m/s^2)");
-                    csvLogger_gyro_1.appendLine(String.format(Locale.getDefault(), "%d,%.6f,%.6f,%.6f,%.6f", gyroResponse.body.timestamp, time/1000,x,y,z));
-                }
-            }
-
-            @Override
-            public void onError(MdsException error) {
-                Log.e(TAG, "subscription onError(): ", error);
-                unsubscribe1_gyro();
-                onDestroy();
-            }
-        });
-    }
-
-    private void subscribeToSensor2(String deviceSerial2) {
-        if (mdsSubscription2_acc != null) {
-            unsubscribe2_acc();
-        }
-
-        String accUri = Constants.URI_MEAS_ACC_52;
-        String strContract_acc_2 = "{\"Uri\": \"" + deviceSerial2 + accUri + "\"}";
-        mdsSubscription2_acc = Mds.builder().build(this).subscribe(Constants.URI_EVENTLISTENER, strContract_acc_2, new MdsNotificationListener() {
-
-            // センサ値を受け取るメソッド
-            @Override
-            public void onNotification(String data) {
-                MovesenseAccDataResponse accResponse = new Gson().fromJson(data, MovesenseAccDataResponse.class);
-                MovesenseGyroDataResponse gyroResponse = new Gson().fromJson(data, MovesenseGyroDataResponse.class);
-                if (accResponse != null && accResponse.body.array.length > 0) {
-                    StringBuffer sb = new StringBuffer();
-                    double time=System.currentTimeMillis()-first_time;
-                    double x = accResponse.body.array[0].x;
-                    double y = accResponse.body.array[0].y;
-                    double z = accResponse.body.array[0].z;
-                    Log.d(TAG,deviceSerial2+"\n経過時刻Time:"+ String.valueOf(time)
-                            +"\nsensorTime:"+String.valueOf(accResponse.body.timestamp)
-                            +"\nx:"+String.valueOf(x)
-                            +"\ny:"+ String.valueOf(y)
-                            +"\nz:"+String.valueOf(z));
-                    sb.append("経過時刻:"+(double)Math.round((double)time/10)/100+"秒");
-                    sb.append("\n");
-                    sb.append("x:"+x);
-                    sb.append("\n");
-                    sb.append("y:"+y);
-                    sb.append("\n");
-                    sb.append("z:"+z);
-                    tvSensorInfo2.setText(sb.toString());
-                    csvLogger_acc_2.appendHeader("Sensor time (ms),System time (ms),X (m/s^2),Y (m/s^2),Z (m/s^2)");
-                    csvLogger_acc_2.appendLine(String.format(Locale.getDefault(), "%d,%.6f,%.6f,%.6f,%.6f", accResponse.body.timestamp, time/1000,x,y,z));
-                }
-            }
-
-            @Override
-            public void onError(MdsException error) {
-                Log.e(TAG, "subscription onError(): ", error);
-                unsubscribe2_acc();
-                onDestroy();
-            }
-        });
-
-        //Movesense1の角速度の登録
-        if (mdsSubscription2_gyro != null) {
-            unsubscribe2_gyro();
-        }
-        String gyroUri = ANGULAR_VELOCITY_PATH + sampleRate;
-        String strContract_gyro_2 =  "{\"Uri\": \"" + deviceSerial2 + gyroUri+  "\"}";
-
-        mdsSubscription2_gyro = Mds.builder().build(this).subscribe(Constants.URI_EVENTLISTENER, FormatHelper.formatContractToJson(deviceSerial2, ANGULAR_VELOCITY_PATH + sampleRate), new MdsNotificationListener() {
-
-            // センサ値を受け取るメソッド
-            @Override
-            public void onNotification(String data) {
-                MovesenseGyroDataResponse gyroResponse = new Gson().fromJson(data, MovesenseGyroDataResponse.class);
-
-                if (gyroResponse != null & gyroResponse.body.array.length > 0) {
-                    double time=System.currentTimeMillis()-first_time;
-                    double x = gyroResponse.body.array[0].x;
-                    double y = gyroResponse.body.array[0].y;
-                    double z = gyroResponse.body.array[0].z;
-                    csvLogger_gyro_1.appendHeader("Sensor time (ms),System time (ms),X (m/s^2),Y (m/s^2),Z (m/s^2)");
-                    csvLogger_gyro_1.appendLine(String.format(Locale.getDefault(), "%d,%.6f,%.6f,%.6f,%.6f", gyroResponse.body.timestamp, time/1000,x,y,z));
-                }
-            }
-
-            @Override
-            public void onError(MdsException error) {
-                Log.e(TAG, "subscription onError(): ", error);
-                unsubscribe2_gyro();
+                unsubscribe2();
                 onDestroy();
             }
         });
 
     }
 
-    private void unsubscribe1_acc() {
-        if (mdsSubscription1_acc != null) {
-            mdsSubscription1_acc.unsubscribe();
-            mdsSubscription1_acc = null;
+//    private void subscribeToSensor2(String deviceSerial2) {
+//        if (mdsSubscription2 != null) {
+//            unsubscribe2();
+//        }
+//
+//        String Uri = Constants.IMU6_PATH;
+//        String strContract2 = "{\"Uri\": \"" + deviceSerial2 + Uri + "\"}";
+//        mdsSubscription2 = Mds.builder().build(this).subscribe(Constants.URI_EVENTLISTENER, strContract2, new MdsNotificationListener() {
+//
+//            // センサ値を受け取るメソッド
+//            @Override
+//            public void onNotification(String data) {
+//                ImuModel imuModel = new Gson().fromJson(data, ImuModel.class);
+//                double time=System.currentTimeMillis()-first_time;
+//                double x = imuModel.getBody().getArrayAcc()[0].getX();
+//                double y = imuModel.getBody().getArrayAcc()[0].getY();
+//                double z = imuModel.getBody().getArrayAcc()[0].getZ();
+//                double gx = imuModel.getBody().getArrayAcc()[0].getX();
+//                double gy = imuModel.getBody().getArrayAcc()[0].getY();
+//                double gz = imuModel.getBody().getArrayAcc()[0].getZ();
+//                Log.d(TAG,deviceSerial2+"\n経過時刻Time:"+ String.valueOf(time)
+//                        +"\nsensorTime:"+String.valueOf(accResponse.body.timestamp)
+//                        +"\nx:"+String.valueOf(x)
+//                        +"\ny:"+ String.valueOf(y)
+//                        +"\nz:"+String.valueOf(z));
+////                sb.append("経過時刻:"+(double)Math.round((double)time/10)/100+"秒");
+////                sb.append("\n");
+////                sb.append("x:"+x);
+////                sb.append("\n");
+////                sb.append("y:"+y);
+////                sb.append("\n");
+////                sb.append("z:"+z);
+//                //tvSensorInfo2.setText(sb.toString());
+//                csvLogger2.appendHeader("Sensor time (ms),System time (ms),X (m/s^2),Y (m/s^2),Z (m/s^2)");
+//                csvLogger2.appendLine(String.format(Locale.getDefault(), "%d,%.6f,%.6f,%.6f,%.6f", accResponse.body.timestamp, time/1000,x,y,z));
+//            }
+//
+//            @Override
+//            public void onError(MdsException error) {
+//                Log.e(TAG, "subscription onError(): ", error);
+//                unsubscribe2();
+//                onDestroy();
+//            }
+//        });
+//
+//    }
+
+    private void unsubscribe1() {
+        if (mdsSubscription1 != null) {
+            mdsSubscription1.unsubscribe();
+            mdsSubscription1 = null;
         }
     }
 
-    private void unsubscribe1_gyro() {
-        if (mdsSubscription1_gyro != null) {
-            mdsSubscription1_gyro.unsubscribe();
-            mdsSubscription1_gyro = null;
-        }
-    }
-
-    private void unsubscribe2_acc() {
-        if(mdsSubscription2_acc != null){
-            mdsSubscription2_acc.unsubscribe();
-            mdsSubscription2_acc = null;
-        }
-    }
-
-    private void unsubscribe2_gyro() {
-        if (mdsSubscription2_gyro != null) {
-            mdsSubscription2_gyro.unsubscribe();
-            mdsSubscription2_gyro = null;
+    private void unsubscribe2() {
+        if (mdsSubscription2 != null) {
+            mdsSubscription2.unsubscribe();
+            mdsSubscription2 = null;
         }
     }
 
